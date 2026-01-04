03bcb2048af77c0647762137fa0e5c317037f90e
Kevin <ConvexBurrito5@gmail.com>
2026-01-04 18:58:33 -0500

    update

--- Codebase/Collection/iq2.m
+++ Codebase/Collection/iq2.m
@@ -2,7 +2,7 @@
 
 % Define file name and parameters
 data_dir = 'C:\Users\steve\PycharmProjects\TVWS\Data';
-filename = fullfile(data_dir, '2026-01-04T23-56-16_4025_capture_1.iq');  % Full path to the IQ capture
+filename = fullfile(data_dir, '2026-01-04T23-57-11_5812_capture_1.iq');  % Full path to the IQ capture
 fs = 20e6;  % HackRF sample rate (20 MHz)
 fc = 491e6;  % Center frequency (MHz for reference)
 
