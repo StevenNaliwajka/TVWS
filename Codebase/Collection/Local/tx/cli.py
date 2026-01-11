6ebfbbc17a2480415cf057af319ef6652f906778
Kevin <ConvexBurrito5@gmail.com>
2026-01-11 10:15:19 -0500

    update

--- Codebase/Collection/Local/tx/cli.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from typing import List, Optional
-from Codebase.Collection.Local.tx.controller_lib import main as tx_main
-
-def main(argv: Optional[List[str]] = None) -> int:
-    return tx_main(argv)
-
-if __name__ == "__main__":
-    raise SystemExit(main())
